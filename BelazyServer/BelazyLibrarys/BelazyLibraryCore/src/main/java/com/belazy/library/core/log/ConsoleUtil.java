892453b76c15bf0922a810404903068160b62637
tangcp <tangchupeng@163.com>
2021-03-05 15:14:09 +0800

    控制台日志输出

---
+++ BelazyServer/BelazyLibrarys/BelazyLibraryCore/src/main/java/com/belazy/library/core/log/ConsoleUtil.java
@@ -0,0 +1,54 @@
+package com.belazy.library.core.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author tangcp
+ */
+@Slf4j
+public class ConsoleUtil {
+    public static void startBasicServerLog(ApplicationContext applicationContext)  {
+        Environment env = applicationContext.getEnvironment ();
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        String port = env.getProperty("server.port");//端口号
+        String appName = env.getProperty("spring.application.name");//服务名称
+        String ipAddress = null;//IP地址
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace ();
+            log.info ("ipAddress:{}","地址获取不到");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "----------------------------------------------------------",
+                appName, ipAddress, port);
+    }
+
+    public static void startBusinessServerLog(ApplicationContext applicationContext)  {
+        Environment env = applicationContext.getEnvironment ();
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        String port = env.getProperty("server.port");//端口号
+        String appName = env.getProperty("spring.application.name");//服务名称
+        String ipAddress = null;//IP地址
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace ();
+            log.info ("ipAddress:{}","地址获取不到");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "SwaggerUI: \thttp://localhost:{}/swagger-ui/index.html \n\t" +
+                        "Knife4jUI: \thttp://localhost:{}/doc.html\n" +
+                        "----------------------------------------------------------",
+                appName, ipAddress, port, port,port);
+    }
+}
