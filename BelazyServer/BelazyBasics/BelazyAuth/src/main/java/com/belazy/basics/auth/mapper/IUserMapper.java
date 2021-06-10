330aa8a3aa34e02f7730b47af9ba26dcb81a1f8b
tangcp <tangchupeng@163.com>
2021-06-10 11:27:08 +0800

    数据结构调整

--- BelazyServer/BelazyBasics/BelazyAuth/src/main/java/com/belazy/basics/auth/mapper/IUserMapper.java
+++ BelazyServer/BelazyBasics/BelazyAuth/src/main/java/com/belazy/basics/auth/mapper/IUserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
  * @author tangcp
  */
 public interface IUserMapper {
-    String SQL = "SELECT u.id  , u.account,u.phone_number,u.email , u.`password` , u.is_account_enabled , u.is_account_expired , u.is_account_locked , u.is_credentials_expired FROM sys_login_account u WHERE u.is_deleted = 0 AND \t";
+    String SQL = "SELECT u.id  , u.account,u.phone_number,u.email , u.`password` , u.is_account_enabled , u.is_account_expired , u.is_account_locked , u.is_credentials_expired FROM sys_user u WHERE u.is_deleted = 0 AND \t";
 
     @Select(SQL + "u.account=#{account}")
     UserDetail findUserByAccount(String account);
